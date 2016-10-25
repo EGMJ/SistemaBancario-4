93d1b4d19bb7b96062df3200e6fc7afd06e8c084
Jefry0727 <jjmb2789@gmail.com>
2016-10-25 16:20:18 -0500

    estructura

---
+++ SistemaBancario/Platfom.Object/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platfom.Object
+{
+    public class Class1
+    {
+    }
+}
