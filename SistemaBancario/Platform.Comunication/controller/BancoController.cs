b29d0cf70abb75279ffb9ef75f1da512e2262a89
Alvaro021095 <lotero021095@gmail.com>
2016-11-01 00:33:49 -0500

    Oeeee

---
+++ SistemaBancario/Platform.Comunication/controller/BancoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Platform.Object.model;
+using Platform.Service.services;
+namespace Platform.Comunication.controller
+{
+    class BancoController
+    {
+        //Metodo para recibir datos y enviarlos al dao
+        public bool SolicitudGuardarBanco(String nit, String nombre, String desc)
+        {
+            Banco banco = new Banco(nit, nombre, desc); 
+            BancoDAO banDAO = new BancoDAO();
+            return banDAO.guardarBanco(banco);   
+        }
+
+    }
+}
